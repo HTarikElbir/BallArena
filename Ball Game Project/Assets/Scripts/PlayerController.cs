9b8f2758dd2e1ec392dcb8d9147da2ff403c4ca2
Halil Tarık ELBİR <ayvakurbisi@gmail.com>
2023-08-13 14:09:38 +0300

    Added Powerup Indicator

--- Ball Game Project/Assets/Scripts/PlayerController.cs
+++ Ball Game Project/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public bool hasPowerup;
     private float powerupStrength = 15.0f;
-    // public GameObject powerupIndicator;
+    public GameObject powerupIndicator;
   
     // Start is called before the first frame update
     void Start()
@@ -23,12 +23,14 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed *-1);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, 1, 0);
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountDownRoutine());
 
@@ -38,6 +40,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
     private void OnCollisionEnter(Collision collision)
     {
